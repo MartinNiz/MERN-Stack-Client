d33bb711440a253ff7a7a403a5aa47b6531e80d2
MartinNiz <martin.niz.niz@gmail.com>
2022-11-26 12:21:40 -0300

    form singup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import ListUser from './usuario/ListUser';
 import AddUser from './usuario/AddUser';
 import EditUser from './usuario/EditUser';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <h1>CRUD en react</h1>
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid ">
+          <a className="navbar-brand" href={ListUser}>MERN</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link" aria-current="page" href={AddUser}>Home</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href={EditUser}>Link</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       
       <BrowserRouter>
         <Routes>
--- src/usuario/AddUser.js
+++ src/usuario/AddUser.js
@@ -1,8 +1,44 @@
 import React from 'react';
+
+  function addUser() {
+    
+  }
+
 function AddUser(){
   return(
-    <div>
-      <h2>Crear Usuario</h2>
+    <div className='container'>
+      <h2 className='m-5'>Create User</h2>
+      <div className='row'>
+        <div className='col-6 offset-3'>
+          <div className="card" style={{width: "18rem;"}}>
+            <div className="card-body">
+              <form className='control-form'>
+              <label for="UserName" className="form-label">User Name</label>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon3"><i className='fa fa-user'></i></span>
+                <input type="text" className="form-control" id="UserName" name='user_name' aria-describedby="basic-addon3"></input>
+              </div>
+
+              <label for="email" className="form-label">Email</label>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon3"><i className='fa fa-envelope'></i></span>
+                <input type="text" className="form-control" id="email" name='email' aria-describedby="basic-addon3"></input>
+              </div>
+
+              <label for="phone" className="form-label">Phone</label>
+              <div className="input-group mb-3">
+                <span className="input-group-text" id="basic-addon3"><i className='fa fa-phone'></i></span>
+                <input type="text" className="form-control" id="phone" name='phone' aria-describedby="basic-addon3"></input>
+              </div>
+
+              <button onClick={addUser} className='btn btn-light border-black'>Guardar</button>
+
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
